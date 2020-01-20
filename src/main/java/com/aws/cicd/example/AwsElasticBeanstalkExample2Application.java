5ed526b4c7582f2f21623635b34282d5106e2796
sanarula <sanarula@SANARULA-IN.in.oracle.com>
2020-01-20 14:57:42 +0530

    first commit

---
+++ src/main/java/com/aws/cicd/example/AwsElasticBeanstalkExample2Application.java
@@ -0,0 +1,13 @@
+package com.aws.cicd.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsElasticBeanstalkExample2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsElasticBeanstalkExample2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/aws/cicd/example/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.aws.cicd.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/hello")
+@RestController
+public class HelloController {
+
+    @GetMapping
+    public String hello() {
+        return "Hello AWS CICD";
+    }
+}
